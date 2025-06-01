029d177fccfd50f22c6bec57e93cdc9c9b43804a
JuniorJose-git <joseronaldosantus@gmail.com>
2025-06-01 16:36:56 -0300

    Adicionado protótipo de player e mapa.

---
+++ scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+@onready var animation := $AnimatedSprite2D as AnimatedSprite2D 
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("ui_left", "ui_right","ui_up","ui_down")
+	velocity = direction * SPEED
+	
+	if direction:
+		animation.play("run2")
+		if direction[0]:
+			animation.scale.x = round(direction[0])
+	else:
+		animation.play("idle")
+
+	move_and_slide()
